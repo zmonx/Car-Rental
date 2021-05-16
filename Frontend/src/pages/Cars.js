8e0c882c7c31d93b73dda39fe0eaf05b41fc8f19
wic0144 <arm_ruttanard@hotmail.com>
2021-05-16 22:40:34 +0700

    add modal && add lottie handle insert modal

--- Frontend/src/pages/Cars.js
+++ Frontend/src/pages/Cars.js
@@ -1,7 +1,9 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import axios from 'axios';
-
+import Lottie from 'react-lottie';
+import animationData from '../lotties/25300-plus-button';
+import Modal from '@material-ui/core/Modal';
 
 
 
@@ -32,58 +34,65 @@ export default class Cars extends Component {
       this.state.cars &&
       this.state.cars.map(cars => (
         <div className="col-lg-4 col-md-6 mb-4">
-            <div className="item-1">
-              <Link to="detail">
-                <img
-                  src={cars.img}
-                  alt="Image"
-                  className="img-fluid"
-                  style={{width:"350px"}}
-                />
-              </Link>
-              <div className="item-1-contents">
-                <div className="text-center">
-                  <h3>
-                    <Link to="detail">{cars.Brand} {cars.Modal}</Link>
-                  </h3>
-                  <div className="rating">
-                    <span className="icon-star text-warning"></span>
-                    <span className="icon-star text-warning"></span>
-                    <span className="icon-star text-warning"></span>
-                    <span className="icon-star text-warning"></span>
-                    <span className="icon-star text-warning"></span>
-                  </div>
-                  <div className="rent-price">
-                    <span>{cars.Price_day}/</span>day
-                  </div>
+          <div className="item-1">
+            <Link to={{
+              pathname: '/detail',
+              state: { cars_id: cars._id }
+            }}>
+              <img
+                src={cars.img}
+                alt="Image"
+                className="img-fluid"
+                style={{ width: "350px" }}
+              />
+
+            </Link>
+            <div className="item-1-contents">
+              <div className="text-center">
+                <h3>
+                  <Link to={{
+                    pathname: '/detail',
+                    state: { cars_id: cars }
+                  }}>{cars.Brand} {cars.Modal}</Link>
+                </h3>
+                <div className="rating">
+                  <span className="icon-star text-warning"></span>
+                  <span className="icon-star text-warning"></span>
+                  <span className="icon-star text-warning"></span>
+                  <span className="icon-star text-warning"></span>
+                  <span className="icon-star text-warning"></span>
                 </div>
-                <ul className="specs">
-                  <li>
-                    <span>Doors</span>
-                    <span className="spec">{cars.Doors}</span>
-                  </li>
-                  <li>
-                    <span>Seats</span>
-                    <span className="spec">{cars.Seats}</span>
-                  </li>
-                  <li>
-                    <span>Transmission</span>
-                    <span className="spec">{cars.Transmission}</span>
-                  </li>
-                  <li>
-                    <span>Minium age</span>
-                    <span className="spec">18 years</span>
-                  </li>
-                </ul>
-                <div className="d-flex action">
-                  <Link to="/contact" className="btn btn-primary">
-                    Rent Now
+                <div className="rent-price">
+                  <span>{cars.Price_day}/</span>day
+                  </div>
+              </div>
+              <ul className="specs">
+                <li>
+                  <span>Doors</span>
+                  <span className="spec">{cars.Doors}</span>
+                </li>
+                <li>
+                  <span>Seats</span>
+                  <span className="spec">{cars.Seats}</span>
+                </li>
+                <li>
+                  <span>Transmission</span>
+                  <span className="spec">{cars.Transmission}</span>
+                </li>
+                <li>
+                  <span>Minium age</span>
+                  <span className="spec">18 years</span>
+                </li>
+              </ul>
+              <div className="d-flex action">
+                <Link to="/contact" className="btn btn-primary">
+                  Rent Now
                   </Link>
-                </div>
               </div>
             </div>
           </div>
-       
+        </div>
+
       ))
     );
   }
@@ -93,7 +102,7 @@ export default class Cars extends Component {
   //   {
   //     this.state.cars.map((data) => {
   //       return (
-          
+
   //       )
   //     })
   //   }
@@ -103,6 +112,50 @@ export default class Cars extends Component {
 
   render() {
     // let data = this.state.cars;
+    const defaultOptions = {
+      loop: true,
+      autoplay: true,
+      animationData: animationData,
+      rendererSettings: {
+        preserveAspectRatio: 'xMidYMid slice'
+      }
+    };
+    const body = (
+
+      <div className="row align-items-center justify-content-center mt-5 p-3">
+        <div className="col-6 feature-car-rent-box-1 ">
+          <h3>Range Rover S7</h3>
+          <ul className="list-unstyled">
+            <li>
+              <span>Doors</span>
+              <span className="spec">4</span>
+            </li>
+            <li>
+              <span>Seats</span>
+              <span className="spec">6</span>
+            </li>
+            <li>
+              <span>Lugage</span>
+              <span className="spec">2 Suitcase/2 Bags</span>
+            </li>
+            <li>
+              <span>Transmission</span>
+              <span className="spec">Automatic</span>
+            </li>
+            <li>
+              <span>Minium age</span>
+              <span className="spec">Automatic</span>
+            </li>
+          </ul>
+          <div className="d-flex align-items-center bg-light p-3">
+            <span>$150/day</span>
+            <Link to="/contact" className="ml-auto btn btn-primary">
+              Rent Now
+                      </Link>
+          </div>
+        </div></div>
+
+    );
     return (
       <div>
         <div className="site-wrap" id="home-section">
@@ -135,9 +188,35 @@ export default class Cars extends Component {
           <div className="site-section bg-light">
             <div className="container">
               <div className="row">
-  
-                {this.showProducts()}
 
+                {/* //////////////////////////// */}
+
+
+
+                {/* //////////////////////////// */}
+                {this.showProducts()}
+                <div className="col-lg-4 col-md-6 mb-4">
+                  <div className="item-1">
+                    <div className="item-1-contents">
+                      <Lottie options={defaultOptions}
+                        height={300}
+                        width={300}
+                        isStopped={false}
+                        isPaused={false} />
+                    </div>
+                  </div>
+                  <div>
+                    <Modal
+                      open={true}
+                      onClose={false}
+                      aria-labelledby="simple-modal-title"
+                      aria-describedby="simple-modal-description"
+                    >
+                      {body}
+                    </Modal>
+                  </div>
+
+                </div>
                 <div className="col-12">
                   <span className="p-3">1</span>
                   <Link to="/#" className="p-3">
--- Frontend/src/pages/Detail.js
+++ Frontend/src/pages/Detail.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
 
 class Detail extends Component {
+
     render() {
+
+        const carThis = this.props.location.state.cars
+
+
         return (
             <div>
-                
+                <p>xxxxxxxxxxxxxxxxxxxxxxx{carThis._id}</p>
+
+
             </div>
         );
     }
+
 }
 
 export default Detail;
