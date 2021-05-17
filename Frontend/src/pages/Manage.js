b39b86bc535e6d212cb48d06878dd41716a023e7
wic0144 <arm_ruttanard@hotmail.com>
2021-05-18 02:16:52 +0700

    fix page manage

--- Frontend/src/pages/Manage.js
+++ Frontend/src/pages/Manage.js
@@ -3,14 +3,20 @@ import axios from 'axios';
 import React, { Component } from 'react'
 import { FaPencilAlt } from 'react-icons/fa';
 import { FaPlus } from 'react-icons/fa';
-import { FaSearch} from 'react-icons/fa';
+import { FaSearch } from 'react-icons/fa';
 export default class Manage extends Component {
 
 
 
-    state = {
-        car: ""
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            car: ""
+
+        }
     }
+
 
 
     componentDidMount() {
@@ -26,9 +32,9 @@ export default class Manage extends Component {
     }
 
 
-    delete(id){
+    delete(id) {
         console.log("DidMount");
-        axios.get("http://localhost:8000/delete/"+id)
+        axios.get("http://localhost:8000/delete/" + id)
             .then(response => {
                 console.log(response.data);
                 this.setState({ car: response.data });
@@ -37,31 +43,31 @@ export default class Manage extends Component {
                     console.log(error);
                 });
         window.location.reload(false);
-        console.log("test",id);
+        console.log("test", id);
     }
 
 
     showProducts() {
         return (
             this.state.car &&
-            this.state.car.map((cars, index)=> (
+            this.state.car.map((cars, index) => (
                 <tbody id="myTable">
-                        <tr>
-                            <td>{index+1}</td>
-                            <td>{cars.Brand}</td>
-                            <td>{cars.Modal}</td>
-                            <td>{cars.Price_day}</td>
-                            <td>
-                                {/* <a href="" class="btn btn-warning"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Edit</a> */}
-                                {/* <Link class="btn btn-warning" to={{    pathname: "/cars",    state: cars._id   }}> Edit </Link> */}
-                                <Link class="btn btn-warning" to={{pathname: '/cars', state: { cars_id: cars }}}>Edit</Link>
-                                {/* <button class="btn btn-danger" onClick={() => this.delete(cars._id)}> Delete</button> */}
-                                <button class="btn btn-danger" onClick={(e) => { if (window.confirm('Are you sure you wish to delete this item?')) this.delete(cars._id) } }> Delete</button>
+                    <tr>
+                        <td>{index + 1}</td>
+                        <td>{cars.Brand}</td>
+                        <td>{cars.Modal}</td>
+                        <td>{cars.Price_day}</td>
+                        <td>
+                            {/* <a href="" class="btn btn-warning"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Edit</a> */}
+                            {/* <Link class="btn btn-warning" to={{    pathname: "/cars",    state: cars._id   }}> Edit </Link> */}
+                            <Link class="btn btn-warning" to={{ pathname: '/cars', state: { cars: cars } }}>Edit</Link>
+                            {/* <button class="btn btn-danger" onClick={() => this.delete(cars._id)}> Delete</button> */}
+                            <button class="btn btn-danger" onClick={(e) => { if (window.confirm('Are you sure you wish to delete this item?')) this.delete(cars._id) }}> Delete</button>
 
-                            </td>
-                        </tr>
-                    </tbody>
-              
+                        </td>
+                    </tr>
+                </tbody>
+
             ))
         );
     }
@@ -96,33 +102,33 @@ export default class Manage extends Component {
                 </div>
 
                 <div
-                        // className="ftco-cover-1 overlay innerpage"
-                        style={{ backgroundImage: "url(images/wpp4.jpg" }}
-                    >
-                <div className="container">
-                    <br></br>  <br></br>  <br></br>
-                    <div className="row">
-                        <div className="col-md-10">
-                           <input className="form-control"  id="myInput" type="text"  placeholder="Search.." />
+                    // className="ftco-cover-1 overlay innerpage"
+                    style={{ backgroundImage: "url(images/wpp4.jpg" }}
+                >
+                    <div className="container">
+                        <br></br>  <br></br>  <br></br>
+                        <div className="row">
+                            <div className="col-md-10">
+                                <input className="form-control" id="myInput" type="text" placeholder="Search.." />
+                            </div>
+                            <div className="col-md-2">
+                                <a href="/insert" className="btn btn-primary btn-block"><FaPlus /> New Product</a>
+                            </div>
                         </div>
-                        <div className="col-md-2">
-                            <a href="/insert" className="btn btn-primary btn-block"><FaPlus/> New Product</a>
-                        </div>
+                        <br></br>  <br></br>
+                        <table className="table table-bordered   text-center  table-hover table-light ">
+                            <thead class="thead-light">
+                                <tr>
+                                    <th>#</th>
+                                    <th>Brand</th>
+                                    <th>Model</th>
+                                    <th>Price/Day</th>
+                                    <th></th>
+                                </tr>
+                            </thead>
+                            {this.showProducts()}
+                        </table>
                     </div>
-                    <br></br>  <br></br>
-                    <table className="table table-bordered   text-center  table-hover table-light ">
-                    <thead class="thead-light">
-                        <tr>
-                            <th>#</th>
-                            <th>Brand</th>
-                            <th>Model</th>
-                            <th>Price/Day</th>
-                            <th></th>
-                        </tr>
-                    </thead>
-                    {this.showProducts()}
-                    </table>
-                </div>
                 </div>
                 {/* <br></br>  <br></br>  <br></br>  <br></br>  <br></br>  <br></br>  <br></br>  <br></br>  <br></br>  <br></br>  <br></br>  <br></br>  <br></br> */}
             </div>
